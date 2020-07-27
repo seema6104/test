35d0f98832736983a2b38bd4f4609106d548771a
seema <seema@192.168.0.38>
2020-07-26 21:49:42 -0400

    commiting

---
+++ ProjectMMP/src/org/iit/util/TestBase.java
@@ -0,0 +1,46 @@
+package org.iit.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.annotations.AfterTest;
+
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	                        
+	 public String url1 = "http://96.84.175.78/MMP-Release1-Integrated-Build.2.4.000/";
+     public String url2= "http://96.84.175.78/MMP-Release1-Integrated-Build.2.4.000/portal/profile.php";
+	 public WebDriver driver;
+	   @BeforeTest
+	   public void launchdriver() throws Exception {
+	    WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	   //WebDriverManager.firefoxdriver().setup();
+	  //driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.get(url1);
+	    
+    	}
+
+	@AfterTest
+    public void closeBrowser() throws Exception {
+    	Thread.sleep(5000);
+    	driver.quit();
+    	
+    	
+    	 
+    }
+ 
+   
+   
+    }
